fafabc5af0279634717021bef152d9a511709def
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2024-02-28 22:59:52 +0500

    Disable assets

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 bundle install
 bundle exec rake db:migrate
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
