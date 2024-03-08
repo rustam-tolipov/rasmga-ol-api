69036f9c91129b9444a8ff4811192eacc2307071
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2024-03-08 17:32:52 +0500

    Load follow avatar

--- app/serializers/follow_serializer.rb
+++ app/serializers/follow_serializer.rb
@@ -1,3 +1,11 @@
 class FollowSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :avatar
+
+  def avatar
+    if object.avatar.present?
+      object.avatar.url
+    else
+      "https://ui-avatars.com/api/?name=#{object.username}&background=random"
+    end
+  end
 end
