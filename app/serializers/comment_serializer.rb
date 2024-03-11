e55b87049458b4b0ad89d48e133defcdbd7f38ca
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2024-03-12 01:42:21 +0500

    Add avatar to CommentSerializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :created_at, :user_id, :post_id, :likes
+  attributes :id, :content, :created_at, :user_id, :post_id, :likes, :avatar
 
   has_many :likes
+
+  def avatar
+    object.user.avatar.url || "https://ui-avatars.com/api/?name=#{object.user.username}&background=random"
+  end
 end
